85219d3a18bcf45e7714ce185632d9da6b12b7a9
Denys Makene <85587960+Denysmak@users.noreply.github.com>
2022-07-29 11:45:31 -0300

    Update projeto22.js

--- projeto22.js
+++ projeto22.js
@@ -1,10 +1,10 @@
-let  SomaDePaginas = 0
+let  somaDeLivros = 0
 class Livro {
     constructor(nome, editora, paginas){
         this.nome = nome
         this.editora = editora
         this.paginas = paginas
-        SomaDePaginas += paginas
+        somaDeLivros++
     }
     anunciarTitulo(){
         console.log(`Título: ${this.nome}`)
@@ -18,4 +18,4 @@ const HA = new Livro("Homem Aranha", "Marvel", 200)
 const SM = new Livro("SuperMan", "DC", 300)
 const CA = new Livro("Capitão America", "Marvel", 200)
 
-console.log("O numero total de paginas de todos os livros é igual a " + SomaDePaginas)
+console.log("O numero de objetos criados foi: " + somaDeLivros)
