87cf6011363e89e828e2d2abbedd20825b0ea49b
Denysmak <denysmakene13@gmail.com>
2022-08-01 20:27:35 -0300

    projeto 23

---
+++ projeto23/projeto23.js
@@ -0,0 +1,31 @@
+
+class carro {
+
+    constructor() {
+        this.modelo = "O modelo ainda não foi informado";
+        this.marca = "A marca ainda não foi informada"
+    }
+    set setModelo(a) {
+        this.modelo = a
+    }
+    get getModelo() {
+        return this.modelo
+    }
+    set setMarca(b) {
+        this.marca = b
+    }
+    get getMarca() {
+        return this.marca
+    }
+}
+
+let c1 = new carro()
+c1.setModelo = "hb20"
+c1.setMarca = "hyundai"
+
+let c2 = new carro()
+c2.setModelo = "palio"
+c2.setMarca = "fiat"
+
+
+
