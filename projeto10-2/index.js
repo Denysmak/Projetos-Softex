cc04da4e2de5d3d6c2526533a9c553524871be92
Denysmak <denysmakene13@gmail.com>
2022-08-22 17:58:53 -0300

    projeto 9-2

---
+++ projeto10-2/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000
+
+app.get("/", (req, res) => {
+    res.send("hello world from GET")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port: ${PORT}`)
+})
